ff92d595183b6dd8e2108c00cbff2cba16bb9494
Mats Becker <mats.becker@gmail.com>
2022-09-04 14:15:29 +0200

    back email css

--- mails/templates/layout.go
+++ mails/templates/layout.go
@@ -56,7 +56,7 @@ const Layout = `
             border: 0;
             cursor: pointer;
             color: #fff !important;
-            background: rgb(28 32 60 / 1) !important;
+            background: #16161a !important;
             text-decoration: none !important;
             line-height: 45px;
             width: 100%;
@@ -67,7 +67,7 @@ const Layout = `
             font-family: Source Sans Pro, sans-serif, emoji;;
             font-size: 14px;
             font-weight: bold;
-            border-radius: 0px;
+            border-radius: 3px;
             box-sizing: border-box;
         }
         .wrapper {
